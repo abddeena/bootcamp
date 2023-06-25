425432d8157409f0c968672a620b13f3c4286860
Dina Abdullah <abddeena7@gmail.com>
2023-06-25 23:16:44 +0300

    chore: next config setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path');
+require('dotenv').config();
+const Dotenv = require('dotenv-webpack');
 
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true
+  },
+  webpack(config) {
+    config.plugins.push(
+      new Dotenv({
+        path: path.join(__dirname, '.env')
+      })
+    );
+    return config;
+  }
+};
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+});
+
+module.exports = withBundleAnalyzer(nextConfig);
